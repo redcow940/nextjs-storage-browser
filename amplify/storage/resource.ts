9226a84382df73b4e0d93bbfa7c03e0de4078a10
redcow940 <67780266+redcow940@users.noreply.github.com>
2025-03-22 16:45:05 +0530

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,7 +1,7 @@
 import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
-  name: "storage-browser-test",
+  name: "lokmat.com",
   access: (allow: any) => ({
     'media-readwritedelete/*': [allow.authenticated.to(['read', 'write', 'delete'])],
     'media-readonly/*': [allow.authenticated.to(['read'])],
