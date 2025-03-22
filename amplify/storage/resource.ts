6aa3fd134f34673a9aca2a3fede09f6b27d604d8
redcow940 <67780266+redcow940@users.noreply.github.com>
2025-03-22 17:20:47 +0530

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,19 +1,5 @@
-import { defineStorage } from "@aws-amplify/backend";
+import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: "lokmatvideo",
-  access: (allow: any) => ({
-    
-    'beta/*': [allow.authenticated.to(['read', 'write', 'delete'])],
-    'contest/*': [allow.authenticated.to(['read'])],
-    'live/*': [ allow.authenticated.to(['read', 'write'])],
-    
-    'protected-useronlyreadwritedelete/{entity_id}/*': [
-      allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
-    'private-useronlyreadwritedelete/{entity_id}/*': [
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-  })
+  name: 'lokmatvideo '
 });
