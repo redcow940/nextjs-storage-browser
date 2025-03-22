ea4a2406e69c53e97e4cd86f56a104e523c1eba1
redcow940 <67780266+redcow940@users.noreply.github.com>
2025-03-22 17:03:54 +0530

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,13 +1,13 @@
 import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
-  name: "storage-browser-test",
+  name: "lokmatvideo",
   access: (allow: any) => ({
-    'media-readwritedelete/*': [allow.authenticated.to(['read', 'write', 'delete'])],
-    'media-readonly/*': [allow.authenticated.to(['read'])],
-    'shared-folder-readwrite/*': [
-      allow.authenticated.to(['read', 'write'])
-    ],
+    
+    'beta/*': [allow.authenticated.to(['read', 'write', 'delete'])],
+    'contest/*': [allow.authenticated.to(['read'])],
+    'live/*': [ allow.authenticated.to(['read', 'write'])],
+    
     'protected-useronlyreadwritedelete/{entity_id}/*': [
       allow.authenticated.to(['read']),
       allow.entity('identity').to(['read', 'write', 'delete'])
