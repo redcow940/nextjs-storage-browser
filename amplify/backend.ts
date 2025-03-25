ab5cd643c485ccb006024604a3a65ba7da2f4bf6
redcow940 <67780266+redcow940@users.noreply.github.com>
2025-03-25 12:20:18 +0530

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,13 +1,13 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
-import { storage } from "./storage/resource";
+import { data } from "./data/resource";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
   auth,
-  storage,
+  data,
 });
 
 backend.addOutput({
