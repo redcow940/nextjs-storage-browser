42b073b5e955f5347a24b30547bd7824ef9987ad
Sanjay Saini <redcow940@gmail.com>
2025-04-12 20:01:29 +0530

    live mvp 0

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,157 @@
+'use client';
+import React, { useState } from "react";
+import { Amplify } from "aws-amplify";
+import './style.css';
+
+import { withAuthenticator } from "@aws-amplify/ui-react";
+
+import config from "../../amplify_outputs.json";
+
+Amplify.configure(config);
+
+const API_ENDPOINT = 'https://oiaei37691.execute-api.ap-south-1.amazonaws.com/live/live';
+
+function Example() {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        company: '',
+        title: '',
+        email: '',
+        subject: '',
+        body: '',
+        to: 'sanjay.saini@lokmat.com', // 👈 Add this line
+      });
+
+
+  const [status, setStatus] = useState({ message: '', type: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus({ message: '', type: '' });
+
+    try {
+      const response = await fetch(API_ENDPOINT, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+        setStatus({ message: 'Message sent successfully!', type: 'success' });
+        setFormData({
+          firstName: '',
+          lastName: '',
+          company: '',
+          title: '',
+          email: '',
+          subject: '',
+          body: '',
+          to:'sanjay.saini@lokmat.com'
+        });
+              // ⏳ Wait 5 seconds, then redirect
+      setTimeout(() => {
+        window.location.href = '/';
+      }, 5000);
+      } else {
+        throw new Error(result.message || 'Error sending message');
+      }
+    } catch (error) {
+      setStatus({ message: error.message || 'Error', type: 'error' });
+    }
+  };
+
+
+  return (
+    <>
+  <div className="contactwrapper">
+  <div className="contactwrapper-inner">
+    <h1 className="brandlogo">
+      <img src="https://lokmat.com/static/asera/asera-logo.png" alt="Asera Minings" />
+    </h1>
+    <h2>Contact Us</h2>
+    <form onSubmit={handleSubmit}>
+  <input
+    placeholder="First Name"
+    required
+    type="text"
+    name="firstName"
+    value={formData.firstName}
+    onChange={handleChange}
+  />
+  <input
+    placeholder="Last Name"
+    required
+    type="text"
+    name="lastName"
+    value={formData.lastName}
+    onChange={handleChange}
+  />
+  <input
+    placeholder="Company"
+    required
+    type="text"
+    name="company"
+    value={formData.company}
+    onChange={handleChange}
+  />
+  <input
+    placeholder="Title"
+    required
+    type="text"
+    name="title"
+    value={formData.title}
+    onChange={handleChange}
+  />
+  <input
+    placeholder="Email"
+    required
+    type="email"
+    name="email"
+    value={formData.email}
+    onChange={handleChange}
+  />
+  <input
+    placeholder="Subject"
+    required
+    type="text"
+    name="subject"
+    value={formData.subject}
+    onChange={handleChange}
+  />
+  <textarea
+    name="body"
+    placeholder="Message"
+    rows={5}
+    required
+    value={formData.body}
+    onChange={handleChange}
+  />
+  <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
+    <button type="submit">Submit</button>
+    <button type="button" onClick={() =>window.location.href = '/'}>
+      Close
+    </button>
+  </div>
+  {status.message && (
+    <p style={{ textAlign: 'center', color: status.type === 'error' ? 'red' : 'green' }}>
+      {status.message}
+    </p>
+  )}
+</form>
+
+  </div>
+</div>
+    </>
+  );
+}
+
+// export default withAuthenticator(Example);
+export default Example;
--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,191 @@
 'use client';
-import React from "react";
-import { Amplify } from "aws-amplify";
-import { signOut } from "aws-amplify/auth";
-
-import { Button, withAuthenticator } from "@aws-amplify/ui-react";
+import React, { useState, useEffect, useMemo } from 'react';
+import { signIn, signOut, getCurrentUser, fetchUserAttributes } from 'aws-amplify/auth';
+import { Amplify } from 'aws-amplify';
+import config from '@/amplify_outputs.json';
+// import { useRouter } from 'next/navigation';
 import {
   createStorageBrowser,
   createAmplifyAuthAdapter,
   elementsDefault,
-} from "@aws-amplify/ui-react-storage/browser";
-import "@aws-amplify/ui-react-storage/styles.css";
-import "@aws-amplify/ui-react-storage/storage-browser-styles.css";
+} from '@aws-amplify/ui-react-storage/browser';
+// import { StorageBrowser } from '@aws-amplify/ui-react-storage';
+import '@aws-amplify/ui-react-storage/styles.css';
+import '@aws-amplify/ui-react-storage/storage-browser-styles.css';
+// import { currentAuthenticatedUser } from 'aws-amplify/auth';
+import { useRouter, usePathname } from 'next/navigation';
 
-import config from "@/amplify_outputs.json";
-
+import './login-style.css';
+import './login-style2.css';
 Amplify.configure(config);
 
-function Example() {
-  const { StorageBrowser } = createStorageBrowser({
-    elements: elementsDefault,
-    config: createAmplifyAuthAdapter({
-            options: {
-        defaultPrefixes: [
-          "files/",
-        ],
-      },
-    }),
-  });
+const authAdapter = createAmplifyAuthAdapter({
+  options: {
+    defaultPrefixes: ['files/'],
+  },
+});
+
+const { StorageBrowser } = createStorageBrowser({
+  elements: elementsDefault,
+  config: authAdapter,
+});
+
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [error, setError] = useState('');
+  const [userProfile, setUserProfile] = useState('');
+
+
+  const router = useRouter();
+
+   // Check if the user is signed in when the component mounts
+   useEffect(() => {
+    console.log("----------------");
+    const checkUser = async () => {
+      try {
+        const user = await getCurrentUser();
+        console.log('Already signed in as:', user);
+        setIsSignedIn(true);
+      } catch {
+        setIsSignedIn(false); // not signed in
+      }
+    };
+    checkUser();
+  }, []);
+
+  const pathname = usePathname();
+
+useEffect(() => {
+  if (userProfile === 'reader') {
+    console.log("...............");
+    const removeActionsMenu = () => {
+      const actionsMenu = document.querySelector('.storage-browser__actions-menu');
+      if (actionsMenu) {
+        actionsMenu.remove();
+      }
+    };
+
+    // Run after a slight delay to ensure the DOM is rendered
+    const timeout = setTimeout(removeActionsMenu, 300);
+
+    return () => clearTimeout(timeout);
+  }
+}, [userProfile, pathname]);
+
+
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError('');
+    try {
+      const existingUser = await getCurrentUser();
+      if (existingUser) {
+        setIsSignedIn(true);
+        return;
+      }
+    } catch {
+      // no user signed in, continue to sign in
+    }
+
+    try {
+      const user = await signIn({ username, password });
+      console.log('Signed in:', user);
+      setIsSignedIn(true);
+
+      const attributes = await fetchUserAttributes();
+      setUserProfile(attributes.profile);
+
+// setIsSignedIn(true);
+
+    } catch (err) {
+      console.error('Sign-in error:', err);
+      setError('Failed to sign in. Please check your credentials.');
+    }
+  };
+
+  if (isSignedIn) {
+    console.log('elementsDefault:', elementsDefault);
+
+    return (
+      <>
+      <div style={{ padding: '2rem' }}>
+                <h1 className="brandlogo">
+          <img src="https://lokmat.com/static/asera/asera-logo.png" alt="Asera Minings" />
+        </h1>
+        <StorageBrowser />
+        <button
+          className="amplify-button amplify-button--secondary"
+          onClick={() => {
+            signOut();
+            setIsSignedIn(false);
+          }}
+        >
+          Sign Out
+        </button>
+
+      </div>
+      <p><strong>CONFIDENTIAL</strong><br/>
+The information contained in the below folders is proprietary and confidential information owned by Asera Mining AB. Its use, viewing and sharing is expressly meant for Asera’s employees and consultants and for authorised external parties covered under non-disclosure agreement established between such parties. No part of such information should be copied, downloaded, shared, photographed or disclosed to any person(s), without the written permission from Asera. </p>
+</>
+    );
+  }
 
   return (
-    <>
-      <Button
-        marginBlockEnd="xl"
-        size="small"
-        onClick={() => {
-          signOut();
-        }}
-      >
-        Sign Out
-      </Button>
-      <StorageBrowser />
-    </>
+    <div data-amplify-authenticator="" data-variation="modal">
+      <div data-amplify-container="">
+        <h1 className="brandlogo">
+          <img src="https://lokmat.com/static/asera/asera-logo.png" alt="Asera Minings" />
+        </h1>
+        <div className="info-container">
+          <div className="info-text">
+            <p className="title">WELCOME TO ASERA MINING</p>
+            <p>
+              We are striving to develop European Union's Lithium mineral resources and contribute to its goal of
+              becoming self-reliant in its transition to green mobility and energy solutions.
+            </p>
+            <p className="forinfomation">For more information,</p>
+            <button className="contactus" onClick={() => router.push('/contact-us')}>Contact us</button>
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <fieldset className="amplify-flex" style={{ flexDirection: 'column' }}>
+              <legend className="amplify-visually-hidden">Sign in</legend>
+
+              <div className="amplify-field amplify-textfield">
+                <label className="amplify-label" htmlFor="username">Email</label>
+                <input
+                  id="username"
+                  name="username"
+                  type="email"
+                  placeholder="Enter your Email"
+                  required
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+
+              <div className="amplify-field amplify-textfield amplify-passwordfield">
+                <label className="amplify-label" htmlFor="password">Password</label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="Enter your Password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </fieldset>
+
+            <button className="amplify-button amplify-button--primary" type="submit">
+              Sign in
+            </button>
+
+            {error && <p style={{ color: 'red', marginTop: '10px' }}>{error}</p>}
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
-
-export default withAuthenticator(Example);
